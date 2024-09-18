42b02cfb117f4297a47042b9a7bdc2331b066e1f
Sudharshan <sudharshanj123@gmail.com>
2024-09-18 23:17:54 +0530

    added checkbox feature

--- src/components/Note.jsx
+++ src/components/Note.jsx
@@ -1,10 +1,31 @@
-import React from "react";
+import React, {useState} from "react";
 
 function Note(props){
+
+    const [isChecked, setIsChecked] = useState(false);
+
+    let [time, setTime] = useState('');
+
+    const handleCheckboxChange = () => {
+        setIsChecked(!isChecked);
+        if (!isChecked){
+            const newTime = new Date().toLocaleDateString();
+            setTime(newTime);
+        }else {
+            time = undefined;
+        }
+    };
+
     return <div className="note"> 
             <p>{props.id}</p>
-            <h1>{props.title}</h1>
+            <h1 id="title" style={{ textDecoration: isChecked ? 'line-through' : 'none' }}>{props.title}</h1>
             <p>{props.desc}</p>
+            <input
+                type="checkbox"
+                checked={isChecked}
+                onChange={handleCheckboxChange}
+            />
+            <span id="time">{time}</span>
         </div>;
 }
 
--- src/components/NoteDetails.js
+++ src/components/NoteDetails.js
@@ -2,15 +2,23 @@ const details = [
     {
         id : 1,
         title : "React Introduction",
-         desc : "A JavaScript library"
+        desc : "A JavaScript library"
     },{
         id : 2,
         title : "React Components",
-         desc : "component"
+        desc : "component"
     },{
         id : 3,
         title : "React Props",
-         desc : "custom properties of the Components"
+        desc : "custom properties of the Components"
+    },{
+        id : 4,
+        title : "React Conditional Redering",
+        desc : "Rendering components/elements/texts/styles based on condtion using ternary operator"
+    }, {
+        id : 5,
+        title : "React useState",
+        desc : "To update state dynamically"
     }
 ]
 
