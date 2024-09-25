a9b253b089272af52a9276beb4e9b894fbe2b74b
Sudharshan <sudharshanj123@gmail.com>
2024-09-25 22:17:31 +0530

    applied passing function concept removed checkbox and date feature

--- src/App.jsx
+++ src/App.jsx
@@ -1,23 +1,34 @@
-import React from "react";
+import React, { useState } from "react";
 import Footer from "./components/Footer";
 import Heading from "./components/Header";
 import Note from "./components/Note";
 import details from "./components/NoteDetails";
 
-function createNote(detail) {
-    return <Note 
-        key= {detail.id}
-        id={detail.id}
-        title= {detail.title}
-        desc= {detail.desc}        
-    />;
-}
 
 function App() {
+
+    let [items, setItems] = useState(details);
+
+    function filterNote(id) {
+        setItems(items.filter((detail) => {
+            return detail.id !== id;
+        }));
+    }
+
+    
+
     return <div>
         <Heading></Heading>
         <div className="note-container">
-        {details.map(createNote)}
+        {items.map(detail => {
+            return <Note 
+                key= {detail.id}
+                id={detail.id}
+                title= {detail.title}
+                desc= {detail.desc} 
+                onChecked={filterNote}       
+    />;
+        })}
         {/* <Note title={details[0].title} desc={details[0].desc}></Note>
         <Note title={details[1].title} desc={details[1].desc}></Note>
         <Note title={details[2].title} desc={details[2].desc}></Note> */}
--- src/components/Note.jsx
+++ src/components/Note.jsx
@@ -4,28 +4,33 @@ function Note(props){
 
     const [isChecked, setIsChecked] = useState(false);
 
-    let [time, setTime] = useState('');
+    // let [time, setTime] = useState('');
 
-    const handleCheckboxChange = () => {
+    // const handleCheckboxChange = () => {
+    //     setIsChecked(!isChecked);
+    //     if (!isChecked){
+    //         const newTime = new Date().toLocaleDateString();
+    //         setTime(newTime);
+    //     }else {
+    //         time = undefined;
+    //     }
+    // };
+
+    function handleChecked() {
         setIsChecked(!isChecked);
-        if (!isChecked){
-            const newTime = new Date().toLocaleDateString();
-            setTime(newTime);
-        }else {
-            time = undefined;
-        }
-    };
+        props.onChecked(props.id)
+    }
 
-    return <div className="note"> 
+    return <div className="note" onClick={handleChecked}> 
             <p>{props.id}</p>
             <h1 id="title" style={{ textDecoration: isChecked ? 'line-through' : 'none' }}>{props.title}</h1>
             <p>{props.desc}</p>
-            <input
+            {/* <input
                 type="checkbox"
                 checked={isChecked}
                 onChange={handleCheckboxChange}
             />
-            <span id="time">{time}</span>
+            <span id="time">{time}</span> */}
         </div>;
 }
 
